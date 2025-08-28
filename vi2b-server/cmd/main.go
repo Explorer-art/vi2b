27ce7f4103b0597ecda4815ead32104072aea20c
Truzme_ <green57421@gmail.com>
2025-08-28 13:41:01 +0300

    feat: docs

--- vi2b-server/cmd/main.go
+++ vi2b-server/cmd/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"os"
 	"github.com/Explorer-art/vi2b-server/internal/core"
 )
 
 func main() {
-	server := core.NewServer(":8000", "")
+	address := ":8080"
+
+	if len(os.Args) > 1 {
+		address = os.Args[1]
+	}
+	
+	server := core.NewServer(address, "")
 	server.Start()
 }
